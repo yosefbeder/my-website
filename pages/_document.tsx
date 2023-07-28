90e374e01339ac76a68f44c992e3c9e0fd5f27e9
yosefbeder <dryosefbeder@gmail.com>
2023-07-28 11:18:25 +0300

    Change title

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Yosef Beder</title>
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
