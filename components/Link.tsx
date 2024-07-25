8b6d0b2aa873d474f3702cfd802f380ec31340d0
yosefbeder <dryosefbeder@gmail.com>
2024-07-25 17:59:19 +0300

    Make the design more consistent

--- components/Link.tsx
+++ components/Link.tsx
@@ -11,7 +11,9 @@ interface LinkProps {
 const Link: React.FC<LinkProps> = ({ variant, className, href, children }) => {
   return (
     <NextLink
-      className={`${variant != "icon" ? "px-2 py-1" : "text-current"} ${
+      className={`inline-block ${
+        variant != "icon" ? "px-2 py-1" : "text-current"
+      } ${
         variant == "primary" &&
         "bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 text-white hover:text-white active:text-white rounded-md no-underline"
       } ${className}`}
