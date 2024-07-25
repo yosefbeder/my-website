0f82c4c48f07aeca5d8ea6fb14887145e44354db
yosefbeder <dryosefbeder@gmail.com>
2024-07-25 14:28:23 +0300

    Improve links contrast

--- components/Link.tsx
+++ components/Link.tsx
@@ -13,7 +13,7 @@ const Link: React.FC<LinkProps> = ({ variant, className, href, children }) => {
     <NextLink
       className={`${variant != "icon" ? "px-2 py-1" : "text-current"} ${
         variant == "primary" &&
-        "bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-600 text-white hover:text-white active:text-white rounded-md no-underline"
+        "bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 text-white hover:text-white active:text-white rounded-md no-underline"
       } ${className}`}
       href={href}
     >
