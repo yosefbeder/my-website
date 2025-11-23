2c2c3f52452f6f3710a7397fb124e2bd1467d35f
Yosef Beder <dryosefbeder@gmail.com>
2025-11-23 08:27:23 +0200

    Add /index.html and /index.php redirection to home page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/index.html",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/index.php",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
